e45a08aa75c298640edf642a71ea466061429102
Dmitry Sychev <axiumone@gmail.com>
2020-04-22 18:52:44 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -12,11 +12,10 @@ function callMe() {
 function crazy() {
   // fix the code in here:
 
-
-  thisIsCrazy = function (){
+var thisIsCrazy = function (){
     console.log("hey!!!")
   }
-  var thisIsCrazy
+thisIsCrazy();
 }
 
 function sayMyName() {
